958590c697cd55c21ed3c8ef04397b050360fe4b
ejPark <qdw0313@khu.ac.kr>
2023-05-30 13:20:47 +0900

    HW11: Django 템플릿과 css 꾸미기

--- django_project/urls.py
+++ django_project/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_url project.
+URL configuration for django_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(f"", include("blog.urls"))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+urlpatterns += static(settings.MEDIA_URL, documnet_root=settings.MEDIA_URL)
